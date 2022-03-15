e1fbc02bf996d40834fd08b32b2af1089dc4008a
unknown <mathi.oliveira@hotmail.com>
2022-03-14 21:31:13 -0300

    Desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss"
+import { GenreResponseProps, MovieProps } from "../App"
+import { Button } from "./Button"
+
+interface SiderBarProps {
+    genres: GenreResponseProps[]
+    selectedGenreId: number
+    setSelectedGenreId: Function
+}
+
+export function SideBar(props: SiderBarProps) {
+    function handleClickButton(id: number) {
+        props.setSelectedGenreId(id)
+    }
+
+    return (
+        <nav className="sidebar">
+            <span>
+                Watch<p>Me</p>
+            </span>
+
+            <div className="buttons-container">
+                {props.genres.map((genre) => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => handleClickButton(genre.id)}
+                        selected={props.selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    )
 }
